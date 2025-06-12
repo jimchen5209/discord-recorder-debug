c5b2bc4e02e328f2e9ea8a4769ca48c22e6b6915
jimchen5209 <jimchen5209@gmail.com>
2025-06-12 16:38:57 +0800

    simple recorder

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # discord-recorder-debug
+
+1. install dependencies
+```bash
+yarn install
+```
+2. copy `.env.example` to `.env`
+```bash
+cp .env.example .env
+```
+3. fill in the `.env` file with your Discord bot token and other required information
+4. build the project
+```bash
+yarn build
+```
+5. run the bot
+```bash
+yarn node dist
---
+++ src/index.ts
@@ -0,0 +1,89 @@
+import { Client } from "@projectdysnomia/dysnomia"
+import { Silence } from "./util/Silence"
+import { appendFileSync, existsSync, mkdirSync } from 'fs';
+
+const token = process.env.DISCORD_TOKEN;
+if (!token) {
+  throw new Error("DISCORD_TOKEN environment variable is not set.");
+}
+
+const channelId = process.env.DISCORD_VOICE_CHANNEL_ID;
+if (!channelId) {
+  throw new Error("DISCORD_VOICE_CHANNEL_ID environment variable is not set.");
+}
+
+const client = new Client(token, {
+  restMode: true,
+  gateway: {
+    intents: [
+      "guilds",
+      "guildIntegrations",
+      "guildMessages",
+      "guildVoiceStates",
+      "guildMembers",
+    ],
+  },
+});
+
+client.once("ready", async () => {
+  if (!existsSync("./recordings")) {
+    console.info("Creating recordings directory...");
+    mkdirSync("./recordings", { recursive: true });
+  }
+  
+  console.info(`Logged in as ${client.user.username} (${client.user.id})`);
+  const voiceConnection = await client.joinVoiceChannel(channelId)
+  if (voiceConnection !== undefined) {
+    console.info('Connected, start recording...')
+    voiceConnection.play(new Silence(), { format: 'opusPackets' })
+    
+    voiceConnection.receive('pcm').on('data', (data, user) => {
+      // Save the audio data to a file
+      if (user) {
+        const filePath = `./recordings/${user}.pcm`;
+        if (!existsSync(filePath)) {
+          console.info(`Creating file for user ${user}...`);
+        }
+
+        appendFileSync(filePath, data);
+      }
+    })
+
+    voiceConnection.on('error', (error) => {
+      console.error("Voice connection error:", error);
+    });
+    voiceConnection.on('warn', (message) => {
+      console.warn("Voice connection warning:", message);
+    })
+    voiceConnection.on('debug', (message) => {
+      console.debug("Voice connection debug:", message);
+    })
+  }
+});
+
+client.on("warn", (message) => {
+  console.warn("Warning:", message);
+});
+
+client.on("error", (error) => {
+  console.error("Error:", error);
+});
+
+client.on("debug", (message) => {
+  console.debug("Debug:", message);
+});
+
+process.on('SIGINT', () => {
+  console.info("Received SIGINT. Cleaning up...");
+  client.disconnect({});
+  process.exit(0);
+});
+
+process.on('SIGTERM', () => {
+  console.info("Received SIGTERM. Cleaning up...");
+  client.disconnect({});
+  process.exit(0);
+});
+
+client.connect();
+
